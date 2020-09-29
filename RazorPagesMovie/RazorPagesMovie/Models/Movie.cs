c4a4f4bf408109a0072c34f0d92396e03c046e50
rachel1earnest <rhearnes@geneva,edu>
2020-09-29 17:43:44 -0400

    finished tutorial

---
+++ RazorPagesMovie/RazorPagesMovie/Models/Movie.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RazorPagesMovie.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
+        public string Title { get; set; }
+
+        [Display(Name = "Release Date"), DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z]*$"), Required, StringLength(30)]
+        public string Genre { get; set; }
+
+        [Range(1, 100), DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal Price { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$"), StringLength(5)]
+        public string Rating { get; set; }
+    }
+
+    //The movie class contains:
+    //-the ID field required by database for the primary key
+    //-[DataType(DataType.Date)]: The DataType attribute specifies the type of the data (Date). With this attribute:
+    //--The user is not required to enter time information in the date field.
+    //--Only the date is displayed, not time information.
+}
+
